5b803e55fe46b7488e584b6f9476a389924a996e
hernanOvando <hernan.ovando@gmail.com>
2022-06-18 12:31:36 -0300

    reparado el boton whataapp

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
+
+
+
+
     
-    $('#btn-whatsapp').click(function(e){
-        e.preventDefault();
-       
-        var url = "https://wa.me/5491133710173";    
-        $('#btn-whatsapp').attr('href',url);
-    });
 })
