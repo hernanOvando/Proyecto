0cea7c0b3a632e89c07ec3d8623ef14c4a5a37f4
hernanOvando <hernan.ovando@gmail.com>
2022-06-21 12:00:34 -0300

    añadi el boton subir

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
+$("#btn-subir").click(function (e) { 
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop:0
 
+    },1000);
+    return false
+});
 
 
     
